0f6ca88752a1c5f3b524978cdf39a84840bbdd96
Priyadharshini-Sakthivel <75614879+Priyadharshini-Sakthivel@users.noreply.github.com>
2022-07-21 10:22:06 +0530

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,15 +1,17 @@
 #implementation of xor through neurons
 import numpy as np
-def calculate_layer1(x,w):
-    #reshape x 
-    #find the transpose of w
-    layer1 = # find the net
-    #use bipolar discrete activation function
+def calculate_layer1(x,w1):
+    x = X.reshape(1,3) #reshape x
+    wt = np.transpose(w1) #find the transpose of w
+    layer1 = np.dot(x,wt) # find the net
+    layer1[layer1>0] = 1 #use bipolar discrete activation function
+    layer1[layer1<=0] = -1
     return layer1
 
-def calculate_layer2(out,w):
-    aug_out = #append 1 to the output of layer1
-    layer2 = calculate_layer1(aug_out,w)
+
+def calculate_layer2(out,w2):
+    aug_out = np.append(out,[1]) #append 1 to the output of layer1
+    layer2 = calculate_layer1(aug_out,w2)
     return layer2
 
 
